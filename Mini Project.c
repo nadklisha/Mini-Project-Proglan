c1b920bc7a10ca69a44ae99e3961702c8d4e928d
nadklisha <51150856+nadklisha@users.noreply.github.com>
2019-10-04 14:07:39 +0700

    Update Mini Project.c

--- Mini Project.c
+++ Mini Project.c
@@ -301,3 +301,19 @@ void tigalima(){
 	system("cls");
 	return;
 }
+
+void phr(){
+	int peng;
+	char phr[50];
+	
+	printf("Menghitung PPn\n\n\n");
+	printf("Pajak Hotel dan Restauran adalah Pajak yang dibayar 10 persen dari pengeluaran\n");
+	printf("Masukan pengeluaran : ");
+	scanf("%d", &peng);
+	peng = peng*0.1+peng;
+	gets(phr);
+
+	printf("\nBayar Pajak Hotel dan Restaurant : %d\n", peng);
+	system("pause");
+	return;
+}
